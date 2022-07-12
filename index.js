9019f1ed9f729262f8758a74128fb119ff5fca9e
NotArtwork <AntArce3@gmail.com>
2022-07-12 13:44:34 -0400

    commit tree eeb9c6cbdb00ec1a2fdd26246dfb186c5c92237b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+
+const fetchDogs =  async () => {
+    let req = await fetch('http://localhost:3000/dogs')
+    let res = await req.json
+    return res
+}
+
+const submitData = async (dataObj) => {
+    let req = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body:JSON.stringify(dataObj)
+    })
+}
